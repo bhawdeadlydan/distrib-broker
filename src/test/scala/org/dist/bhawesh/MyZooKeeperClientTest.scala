6904611f28dab9419d1befde26d61be1b78728df
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2020-07-11 12:56:44 +0530

    [Bhawesh] Modify zookeeper registeration specs.

--- src/test/scala/org/dist/bhawesh/MyZooKeeperClientTest.scala
+++ src/test/scala/org/dist/bhawesh/MyZooKeeperClientTest.scala
@@ -7,9 +7,11 @@ class MyZooKeeperClientTest extends ZookeeperTestHarness {
 
   test("should register brokers with zookeeper") {
     val myZookeeperClient = new MyZookeeperClient(zkClient);
+    val myZookeeperClient2 = new MyZookeeperClient(zkClient);
     myZookeeperClient.registerBroker(Broker(0, "10.10.10.10", TestUtils.choosePort()))
     myZookeeperClient.registerBroker(Broker(1, "10.10.10.11", TestUtils.choosePort()))
+    myZookeeperClient.registerBroker(Broker(2, "10.10.10.12", TestUtils.choosePort()))
 
-    assert(2 == myZookeeperClient.getAllBrokers().size)
+    assert(3 == myZookeeperClient2.getAllBrokers().size)
   }
 }
