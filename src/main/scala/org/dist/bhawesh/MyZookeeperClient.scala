19192b9b7c0cac36f1ea756e4fd49e8f66deea30
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2020-07-11 12:26:57 +0530

    [Bhawesh] Register clients with zookeeper and validate it using test zookeeper.

--- src/main/scala/org/dist/bhawesh/MyZookeeperClient.scala
+++ src/main/scala/org/dist/bhawesh/MyZookeeperClient.scala
@@ -1,5 +1,53 @@
 package org.dist.bhawesh
 
-class MyZookeeperClient {
+import org.I0Itec.zkclient.ZkClient
+import org.I0Itec.zkclient.exception.ZkNoNodeException
+import org.dist.simplekafka.common.JsonSerDes
+import org.dist.simplekafka.util.ZkUtils
+import org.dist.simplekafka.util.ZkUtils.Broker
+
+import scala.jdk.CollectionConverters._
+
+class MyZookeeperClient(zkClient: ZkClient) {
+
+  val BrokerIdsPath = "/brokers/ids"
+
+  def getAllBrokers(): Set[Broker] = {
+    zkClient.getChildren(BrokerIdsPath).asScala.map {
+      brokerId: String => {
+        val serialisedBrokerInfo: String = zkClient.readData(getBrokerPath(brokerId.toInt))
+        JsonSerDes.deserialize(serialisedBrokerInfo.getBytes, classOf[Broker])
+      }
+    }.toSet
+  }
+
+  def createParentPath(zkClient: ZkClient, brokerPath: String) = {
+    val parentDirectory = brokerPath.substring(0, brokerPath.lastIndexOf('/'))
+    if (parentDirectory.length != 0) {
+      zkClient.createPersistent(parentDirectory, true)
+    }
+  }
+
+  def createEphemeralPath(zkClient: ZkClient, brokerPath: String, brokerData: String) = {
+    try {
+      zkClient.createEphemeral(brokerPath, brokerData)
+    }
+    catch {
+      case e: ZkNoNodeException =>
+        createParentPath(zkClient, brokerPath)
+        zkClient.createPersistent(brokerPath, brokerData)
+    }
+  }
+
+  def registerBroker(broker: ZkUtils.Broker) = {
+    val brokerData = JsonSerDes.serialize(broker)
+    val brokerPath = getBrokerPath(broker.id)
+    createEphemeralPath(zkClient, brokerPath, brokerData)
+  }
+
+
+  private def getBrokerPath(id: Int) = {
+    BrokerIdsPath + "/" + id
+  }
 
 }
--- src/test/scala/org/dist/bhawesh/MyZooKeeperClientTest.scala
+++ src/test/scala/org/dist/bhawesh/MyZooKeeperClientTest.scala
@@ -1,5 +1,15 @@
 package org.dist.bhawesh
 
-class MyZooKeeperClientTest {
+import org.dist.common.{TestUtils, ZookeeperTestHarness}
+import org.dist.simplekafka.util.ZkUtils.Broker
 
+class MyZooKeeperClientTest extends ZookeeperTestHarness {
+
+  test("should register brokers with zookeeper") {
+    val myZookeeperClient = new MyZookeeperClient(zkClient);
+    myZookeeperClient.registerBroker(Broker(0, "10.10.10.10", TestUtils.choosePort()))
+    myZookeeperClient.registerBroker(Broker(1, "10.10.10.11", TestUtils.choosePort()))
+
+    assert(2 == myZookeeperClient.getAllBrokers().size)
+  }
 }
